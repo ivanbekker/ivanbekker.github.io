fd7612c2e8032cd9a8948730914469220ee1560d
Ivan Bekker <ivan.bekker@corezoid.com>
2020-04-08 21:30:37 +0300

    test

---
+++ test/node_modules/mw-style-react/build/Table/TableCell/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _TableCell = require('./TableCell');
+
+Object.defineProperty(exports, 'default', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_TableCell).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
